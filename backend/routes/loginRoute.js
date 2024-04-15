8dfa369476469d4f791327f01feced5f7a527467
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-04-15 13:10:54 +0530

    created token

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,31 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const db_con = require("../db");
+const secret = "mysecret";
+var jwt = require("jsonwebtoken");
 router.post("/", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
+  const { email } = req.body;
+  const textPassword = req.body.password;
   try {
     db_con.query(
       `SELECT * FROM User WHERE email = "${email}"`,
       (error, results) => {
         if (error) console.log(error);
-        db_con.query(
-          `SELECT * FROM User WHERE email = "${email}" and password = "${password}"`,
-          (err, result) => {
-            if (err) console.log(err);
-
-            if (result) {
-              if (result.length !== 0) {
-                console.log("successfull");
-                res.status(200).json({ message: "successfully" });
-              } else {
-                console.log("not successfull");
-                res.status(301).json({ message: "invalid credentials" });
-              }
-            }
+        const { user_id, password } = results[0];
+        bcrypt.compare(textPassword, password).then((result) => {
+          if (result !== false) {
+            const user = {
+              id: user_id,
+            };
+            const jwtAuthToken = jwt.sign(user, secret);
+            console.log(jwtAuthToken);
+            res.status(201).json({
+              success: true,
+              authToken: jwtAuthToken,
+              message: "Login Successfull",
+            });
+          } else {
+            res
+              .status(400)
+              .json({ success: false, message: "user does not exist" });
           }
-        );
+        });
       }
     );
   } catch (error) {
@@ -34,3 +39,18 @@ router.post("/", (req, res) => {
 });
 
 module.exports = router;
+// db_con.query(
+//   `SELECT * FROM User WHERE email = "${email}" and password = "${password}"`,
+//   (err, result) => {
+//     if (err) console.log(err);
+//     if (result) {
+//       if (result.length !== 0) {
+//         console.log("successfull");
+//         res.status(200).json({ message: "successfully" });
+//       } else {
+//         console.log("not successfull");
+//         res.status(301).json({ message: "invalid credentials" });
+//       }
+//     }
+//   }
+// );
--- backend/routes/signupRoute.js
+++ backend/routes/signupRoute.js
@@ -1,47 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const db_con = require("../db");
-
+const bcrypt = require("bcrypt");
 const path = require("path");
+
 // const pathname = path.join(__dirname, "/usersdata/user.json");
 // const userData = require("./usersdata/user.json");
 // const { error } = require("console");
 
-router.post("/", (req, res) => {
-  const firstName = req.body.firstName.trim();
-  const lastName = req.body.lastName.trim();
+router.post("/", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
   const username = firstName + "_" + lastName;
-  const { email, password } = req.body;
-  try {
-    db_con.query(
-      `INSERT INTO User(username, email, password, role) values ("${username}", "${email}", "${password}", "Regular User" )`,
-      (error, result) => {
-        if (error) console.log(error);
-        console.log("successfully inserted");
+  const salt = await bcrypt.genSalt(10);
+  const secretPassword = await bcrypt.hash(password, salt);
+
+  const sql = `SELECT *   FROM user WHERE email = "${email}"`;
+
+  db_con.query(sql, (error, results) => {
+    if (error) res.status(401);
+    if (results) {
+      if (results.length === 0) {
+        db_con.query(
+          `INSERT INTO user (username, email, password, role, first_name, last_name) values ("${username}", "${email}", "${secretPassword}","${"Regular User"}", "${firstName}", "${lastName}")`,
+          (error, results) => {
+            if (error) console.log(error);
+            if (results) {
+              console.log("Inserted " + results.affectedRows + " row(s)");
+              res.status(201).json({
+                message: "data saved successfully",
+              });
+            }
+          }
+        );
+      } else {
+        console.log("user already existed");
+        res.status(401).json({ message: "user already existed" });
       }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-  // try {
-  //   db_con.query(`select user_id, username from User`, (err, result) => {
-  //     if (err) console.log(err);
-  //     const jsonObj = JSON.stringify(result);
-  //     fs.writeFileSync(pathname, jsonObj, "utf8", (err, data) => {
-  //       if (err) res.status(301).json({ message: "false" });
-  //     });
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  try {
-    db_con.query(`select user_id, username from User`, (err, result) => {
-      if (err) console.log(err);
-      res.send(result);
-    });
-  } catch (error) {
-    res.send(error);
-  }
+    }
+  });
 });
 
 module.exports = router;
