1c24bce13a6773d5c794f5e29467ab5d039d9509
Onkar Patil <onkarvgpatil1998@gmail.com>
2024-07-09 23:44:37 +0530

    try to change name board

--- backend/routes/api/board.js
+++ backend/routes/api/board.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const user_idMiddlewere = require("../../user_idMiddlewere");
+const user_idMiddlewere = require('../../user_idMiddlewere');
 const router = express.Router();
-const boardController = require("../../controllers/boardController");
+const boardcontroller = require('../../controllers/boardcontroller');
 
-router.post("/", boardController.getAllBoard);
+router.post("/", boardcontroller.getAllBoard);
 
-router.post("/create", user_idMiddlewere, boardController.createBoard);
+router.post("/create", user_idMiddlewere, boardcontroller.createBoard);
 
-router.post("/delete", boardController.deleteBoard);
+router.post("/delete", boardcontroller.deleteBoard);
 
 module.exports = router;
