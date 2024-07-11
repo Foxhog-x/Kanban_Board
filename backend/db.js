04ee8448d9ab63acdd5da4c585fbb13a787ac5aa
Onkar Patil <onkarvgpatil1998@gmail.com>
2024-07-11 10:09:33 -0700

    update

--- backend/db.js
+++ backend/db.js
@@ -2,19 +2,19 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 let db_con = mysql.createPool({
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // user: "avnadmin",
-  // password: process.env.DB_PASS,
-  // database: process.env.DATABASE,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "12345",
-  database: "kanban",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: "avnadmin",
+  password: process.env.DB_PASS,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  // host: "127.0.0.1",
+  // port: 3306,
+  // user: "root",
+  // password: "12345",
+  // database: "kanban",
 });
 
 db_con.on("error", (err) => {
